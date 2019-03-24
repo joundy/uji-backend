502ff7c8f0650ce0ea6c6f45659f3f7f8d22631d
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2019-03-24 20:47:08 +0700

    update examLog generate

--- models/examLog.go
+++ models/examLog.go
@@ -10,17 +10,29 @@ import (
 type ExamLog struct {
 	ID        primitive.ObjectID `json:"id" bson:"_id"`
 	UserID    primitive.ObjectID `json:"userId" bson:"userId"`
+	ExamID    primitive.ObjectID `json:"examId" bson:"examId"`
+	Exam      ExamLogExam        `json:"exam" bson:"exam"`
+	Questions []Question         `json:"questions" bson:"questions"`
 	StartTime time.Time          `json:"startTime" bson:"startTime"`
 	EndTime   time.Time          `json:"endTime" bson:"endTime"`
 	Result    ExamLogResult      `json:"result" bson:"result"`
 	IsSubmit  bool               `json:"isSubmit" bson:"isSubmit"`
-	Questions []Question         `json:"questions" bson:"questions"`
 }
 
 //ExamLogResult is represent model for ExamLogResult data
 type ExamLogResult struct {
 	Pass   int `json:"pass" bson:"pass"`
 	Failed int `json:"failed" bson:"failed"`
+}
+
+//ExamLogExam is represent model for ExamLogExam data
+type ExamLogExam struct {
+	Title        string `json:"title" bson:"title"`
+	Description  string `json:"description" bson:"description"`
+	Duration     int    `json:"duration" bson:"duration"`
+	Source       string `json:"source" bson:"source"`
+	Point        int    `json:"point" bson:"point"`
+	PassingGrade int    `json:"passingGrade" bson:"passingGrade"`
 }
 
 //ExamLogG is represent model for ExamLogG data
--- usecase/examLog/examLog.go
+++ usecase/examLog/examLog.go
@@ -2,6 +2,8 @@ package examlog
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/haffjjj/uji-backend/repository/question"
 
@@ -34,10 +36,10 @@ func (c *examLogUsecase) GetByID(i primitive.ObjectID) (*models.ExamLog, error) 
 
 func (c *examLogUsecase) Generate(userID, examID primitive.ObjectID) error {
 
-	// exam, err := c.eRepository.GetByID(examID)
-	// if err != nil {
-	// 	return err
-	// }
+	exam, err := c.eRepository.GetByID(examID)
+	if err != nil {
+		return err
+	}
 
 	filter := models.Filter{Start: 0, Limit: 100}
 	questionGs, err := c.qRepository.FetchG(filter)
@@ -45,9 +47,38 @@ func (c *examLogUsecase) Generate(userID, examID primitive.ObjectID) error {
 		return err
 	}
 
-	questionG := *questionGs[0]
+	questionG := questionGs[0]
+	qDataRaw := questionG.Data
 
-	fmt.Println(questionG.Data)
+	randQuestions(&qDataRaw)
+
+	examLog := models.ExamLog{
+		UserID: userID,
+		Exam: models.ExamLogExam{
+			Title:        exam.Title,
+			Description:  exam.Description,
+			Duration:     exam.Duration,
+			Source:       exam.Source,
+			Point:        exam.Point,
+			PassingGrade: exam.PassingGrade,
+		},
+		StartTime: time.Now(),
+		EndTime:   time.Now().Add(time.Second * time.Duration(exam.Duration)),
+		IsSubmit:  false,
+		Questions: qDataRaw,
+	}
+
+	fmt.Println(examLog)
 
 	return nil
+}
+
+func randQuestions(q *[]models.Question) {
+	rand.Seed(time.Now().UnixNano())
+
+	for i := len(*q) - 1; i >= 0; i-- {
+		r := rand.Intn(len(*q) - 1)
+
+		(*q)[i], (*q)[r] = (*q)[r], (*q)[i]
+	}
 }
